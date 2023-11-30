b17977192e3d29f1506db63b0a24264ea3000c90
Apphely <96137652+apphely@users.noreply.github.com>
2023-11-30 10:53:44 +0300

    Update -> WebP Dönüştür ve Sıkıştır.py
    Açıklamalar

--- WebP Dönüştür ve Sıkıştır.py
+++ WebP Dönüştür ve Sıkıştır.py
@@ -1,8 +1,10 @@
+# .jpg, .png ve .jpeg uzantılı dosyaları .webp'ye dönüştüp sıkıştırma işlemi yaparak alandan tasarruf sağlar. Web siteleri için birebirdir.
+
 from PIL import Image
 import os
 
 # Giriş klasörünü tanımlayın
-giris_klasoru = 'x:\.Çalışmalarım ve Projelerim\Kodlama\Python Projeler\WebP Dönüştürme ve Sıkıştırma/'
+giris_klasoru = 'x:\WebP Dönüştürme ve Sıkıştırma/'
 
 # Giriş klasöründeki tüm dosyaları alın
 dosya_listesi = os.listdir(giris_klasoru)
@@ -15,6 +17,6 @@ for dosya_adı in dosya_listesi:
         # Resmi yükle ve webp'ye dönüştür
         im = Image.open(giris_yolu)
         webp_yolu = os.path.splitext(giris_yolu)[0] + '.webp'
-        im.save(webp_yolu, 'WEBP', quality=20)  # Novel kapakları için kalite 20
+        im.save(webp_yolu, 'WEBP', quality=50)  # Kalite ayarı
 
 print("Tüm fotoğraflar webp formatına dönüştürüldü ve aynı yere kaydedildi.")
