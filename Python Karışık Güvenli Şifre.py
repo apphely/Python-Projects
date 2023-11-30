98a7cc412c52124bfb811e464f3add652e8b8c95
Apphely <96137652+apphely@users.noreply.github.com>
2023-11-30 10:46:31 +0300

    Update -> Python Karışık Güvenli Şifre.py
    Açıklamalar

--- Python Karışık Güvenli Şifre.py
+++ Python Karışık Güvenli Şifre.py
@@ -1,10 +1,11 @@
-while True:
+# Her türlü harfi ve karakteri kullanarak istediğiniz uzunlukta güvenli şifreler oluşturur.
+while True: # Sürekli döngüye sokar.
     import string
     from random import *
 
-    characters = string.ascii_letters + string.punctuation  + string.digits
+    characters = string.ascii_letters + string.punctuation  + string.digits # Harfler, sayılar ve işaretleri kullanarak şifreleme karakterlerini belirler.
 
-    len_characters = int(input("\nŞifrenizin uzunluğu kaç karakter olsun? Yazınız: "))
+    len_characters = int(input("\nŞifrenizin uzunluğu kaç karakter olsun? Yazınız: ")) #Şifre uzunluğu için kullanıcıdan yazmasını ister.
 
     for x in range(len_characters):
         password =  "".join(choice(characters))
